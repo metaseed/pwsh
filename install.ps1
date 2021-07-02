b85a10a37c4ed0aa1edcd2c99eb457b0b284d871
metasong <metaseed@gmail.com>
2021-07-02 10:49:35 -0500

    ok

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 $zip = "temp:pwsh.zip"
 iwr 'http://github.com/metasong/pwsh/archive/refs/heads/master.zip' -OutFile $zip
-Expand-Archive $zip ~/metaseed
+Expand-Archive $zip ~/metaseed -Force
 ri ~/metaseed/pwsh-master ~/metaseed/pwsh
 
 . ~/metaseed/pwsh/set-profile.ps1
