eccbcc48eb7d24b61db99b2ff7e17bc585da456b
metasong <metaseed@gmail.com>
2021-07-02 11:20:10 -0500

    ok

--- Set-Profile.ps1
+++ Set-Profile.ps1
@@ -1,8 +1,11 @@
 
-if (!(Test-Path $profile.CurrentUserAllHosts)) {
+if (Test-Path $profile.CurrentUserAllHosts) {
+  "profile already exist: $($profile.CurrentUserAllHosts)"
+  "append current profile..."
+} else {
+  "create new profile: $($profile.CurrentUserAllHosts)"
   New-Item -ItemType File -Path $profile.CurrentUserAllHosts -Force
-  Add-Content -Path $profile.CurrentUserAllHosts -Value ". $PSScriptRoot\profile.ps1"
-  Write-Host 'profile setup done!'
-  return
 }
-Write-Host "profile already exist: $($profile.CurrentUserAllHosts)"
+Add-Content -Path $profile.CurrentUserAllHosts -Value ". $PSScriptRoot\profile.ps1"
+'profile setup done!'
+
--- install.ps1
+++ install.ps1
@@ -1,6 +1,16 @@
 $zip = "temp:pwsh.zip"
 iwr 'http://github.com/metasong/pwsh/archive/refs/heads/master.zip' -OutFile $zip
-Expand-Archive $zip ~/metaseed -Force
-ri ~/metaseed/pwsh-master ~/metaseed/pwsh
 
+# ~ is better than $env:HomePath, it include the home drive
+Expand-Archive $zip ~/metaseed -Force
+
+# directly ri the pwsh folder may cause error if it is used.
+# mi works even file is used
+# but mi would show error is des exists: because a file or directory with the same name already exists.
+# the .old shouldn't be used/locked by any app
+ri ~/metaseed/.old -Force -Recurse -ErrorAction SilentlyContinue
+mi ~/metaseed/pwsh ~/metaseed/.old -Force -ErrorAction SilentlyContinue
+ri ~/metaseed/.old -Force -Recurse -ErrorAction SilentlyContinue
+
+mi ~/metaseed/pwsh-master ~/metaseed/pwsh -Force
 . ~/metaseed/pwsh/set-profile.ps1
