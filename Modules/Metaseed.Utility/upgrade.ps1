95371151a3515714ed6603ab10d53073538a5ad3
metasong <metaseed@gmail.com>
2021-07-02 13:59:56 -0500

    ok

--- Modules/Metaseed.Utility/upgrade.ps1
+++ Modules/Metaseed.Utility/upgrade.ps1
@@ -11,8 +11,8 @@ function Test-Installation {
     
     $infoLocal = Get-Content $LocalInfoPath | ConvertFrom-Json
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    $infoRemote = iwr $RemoteInfoUrl -TimeoutSec 5 | ConvertFrom-Json
-    return $infoRemote.version -ne $infoLocal.version  
+    $infoRemote = iwr $RemoteInfoUrl -TimeoutSec 10 | ConvertFrom-Json
+    return $infoRemote.version -ne $infoLocal.version ? $infoLocal.version : $false
 }
 function Update-Installation {
     param (
@@ -26,9 +26,10 @@ function Update-Installation {
         [string]
         $InstallUrl
     )
-    if (Test-Installation $LocalInfoPath $RemoteInfoUrl) {
+    $ver = Test-Installation $LocalInfoPath $RemoteInfoUrl
+    if ($ver) {
 
-        write-host "> New version($remoteVersion), press Enter to upgrade, other key to continue"
+        write-host "> New version($ver), press Enter to upgrade, other key to continue"
 
         $key = [System.Console]::ReadKey().Key
         if ($key -eq [ConsoleKey]::Enter) {
