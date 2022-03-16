98737c0c42c27c85c4bd0ae5d3c9714231c415dc
metasong <metaseed@gmail.com>
2022-03-16 09:53:57 -0500

    git

--- Module/Metaseed.Utility/Confirm-Continue.ps1
+++ Module/Metaseed.Utility/Confirm-Continue.ps1
@@ -1,4 +1,4 @@
-function Enter-Continue([string] $mesage) {
+function Confirm-Continue([string] $mesage) {
   Write-Host "`n$message`npress 'Enter' to continue, 'Any key' to cancel...`n" -ForegroundColor blue
   $key = [Console]::ReadKey().Key
   if ($key -ne [ConsoleKey]::Enter) {
