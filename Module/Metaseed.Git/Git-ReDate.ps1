5cc9464cc3de7a24aa86634ec252eab9e7115e07
metasong <metaseed@gmail.com>
2022-03-22 18:25:33 -0500

    ok

--- Module/Metaseed.Git/Git-ReDate.ps1
+++ Module/Metaseed.Git/Git-ReDate.ps1
@@ -14,38 +14,39 @@ function Git-ReDate {
 
 
   )
-  ## mimic interactive way, could modify every commit's author/commit date
+  ### mimic interactive way, could modify every commit's author/commit date
+  # config the rebase command use the sequence.editor notepad, override the one in .gitconfig
+  start-job -scriptblock { git -c sequence.editor=notepad rebase --interactive "head~$using:commits" } -name redate
+
   $git = Find-FromParent .git
   $rebaseFile = "$git/rebase-merge/git-rebase-todo"
-  # config the rebase command use the sequence.editor notepad, override the one in .gitconfig
-  start-job -scriptblock {git -c sequence.editor=notepad rebase --interactive "head~$using:commits"} -name redate
-
-  while(!(Test-Path $rebaseFile)) {
+  while (!(Test-Path $rebaseFile)) {
     Start-Sleep 1
   }
 
-  $line =0;
-  (gc $rebaseFile)|
+  ## modify fit-rebase-todo file
+  $line = 0;
+  (gc $rebaseFile) |
   # pick 4ca564e Do something
   # exec git commit --amend --no-edit --date "1 Oct 2019 12:00:00 PDT"
   % {
-    $date = (Get-date).AddHours($line-$commits).AddMinutes($(get-random -Minimum 0 -Maximum 59)).AddSeconds($(get-random -Minimum 0 -Maximum 59))
-    $_ -replace '(?<pick>^pick.*$)', ('${pick}'+"`nexec git commit --amend --no-edit --date `"$date`"")
+    $date = (Get-date).AddHours($line - $commits).AddMinutes($(get-random -Minimum 0 -Maximum 59)).AddSeconds($(get-random -Minimum 0 -Maximum 59))
+    $_ -replace '(?<pick>^pick.*$)', ('${pick}' + "`nexec git commit --amend --no-edit --date `"$date`"")
     $line++
-  }|
-  
+  } |
   set-content $rebaseFile
-
-  get-process notepad  | ? MainWindowTitle -like 'git-rebase-to*'|% {$_.CloseMainWindow()}
+  ## close editor to continue the git rebase
+  get-process notepad  | ? MainWindowTitle -like 'git-rebase-to*' | % { $_.CloseMainWindow() }
+  ## output results
   Receive-job redate -Wait
-  ##
+  ###
 
-  ## this way the date of commits are the same
+  ### this way the date of commits are the same
   # It changes both the committer and author dates.
   # replay commits from Head~'$commits' with additional git command on every commit
   # date could be: now, 1 day ago, 1 hour ago, 2 hours ago or $(date)
   # git rebase HEAD~"$commits" --exec "git commit --amend --no-edit --date 'now'"
-  ##
+  ###
 
   if (!$NoPush) {
     git push --force
