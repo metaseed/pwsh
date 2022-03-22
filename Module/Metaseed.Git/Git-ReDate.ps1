53f1e343250c0e88e8460f148c6696b351e1b636
metasong <metaseed@gmail.com>
2022-03-22 17:26:35 -0500

    ok

--- Module/Metaseed.Git/Git-ReDate.ps1
+++ Module/Metaseed.Git/Git-ReDate.ps1
@@ -15,24 +15,27 @@ function Git-ReDate {
 
   )
   ## old way not finished, which mimic interactive way
-  # $git = Find-FromParent .git
-  # $rebaseFile = "$git/rebase-merge/git-rebase-todo"
-  # # config the rebase command use the sequence.editor notepad, override the one in .gitconfig
-  # start-job -scriptblock {git -c sequence.editor=notepad rebase --interactive "head~$commits"} -jobname redate
+  $git = Find-FromParent .git
+  $rebaseFile = "$git/rebase-merge/git-rebase-todo"
+  # config the rebase command use the sequence.editor notepad, override the one in .gitconfig
+  start-job -scriptblock {git -c sequence.editor=notepad rebase --interactive "head~$using:commits"} -name redate
 
-  # (gc $rebaseFile)|
-  # # pick 4ca564e Do something
-  # # exec git commit --amend --no-edit --date "1 Oct 2019 12:00:00 PDT"
-  # % {$_ -replace 'pick', 'do some replacement here like above'}|
-  # set-content $rebaseFile
-  # get-process notepad  | ? MainWindowTitle -like 'git-rebase-to*'|% {$_.CloseMainWindow()}
-  # Receive-job redate
+  (gc $rebaseFile)|
+  # pick 4ca564e Do something
+  # exec git commit --amend --no-edit --date "1 Oct 2019 12:00:00 PDT"
+  % {$_ -replace '(?<pick>^pick.*$)', ('${pick}'+"`nexec git commit --amend --no-edit --date `"1 Oct 2019 12:00:00 PDT`"")}|
+  
+  set-content $rebaseFile
+  get-process notepad  | ? MainWindowTitle -like 'git-rebase-to*'|% {$_.CloseMainWindow()}
+  Receive-job redate
   ##
 
+  ## this way the date of commits are the same
   # It changes both the committer and author dates.
   # replay commits from Head~'$commits' with additional git command on every commit
-  # now, 1 day ago, 1 hour ago
-  git rebase HEAD~"$commits" --exec "git commit --amend --no-edit --date `$(date -R -d '2 days ago')"
+  # now, 1 day ago, 1 hour ago, or $(date)
+  git rebase HEAD~"$commits" --exec "git commit --amend --no-edit --date 'now'"
+  ##
 
   if (!$NoPush) {
     git push --force
