9549b9318f6a85d9ff77f53fb1cc00e90035272d
metasong <metaseed@gmail.com>
2022-05-12 10:46:53 -0500

    setup-poshgit

--- Cmdlet/Setup-PoshGit.ps1
+++ Cmdlet/Setup-PoshGit.ps1
@@ -1,9 +1,16 @@
+# with the empty CmdletBinding attribute, -Verbose parameter could be used to show more information.
+[CmdletBinding()]
+param (  
+)
+
 Import-Module posh-git -ErrorAction SilentlyContinue
 
 if($?) {
   "update posh-git..."
   PowerShellGet\Update-Module posh-git
+  "update posh-git done"
 } else {
   "install posh-git..."
   PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
+  "install posh-git done"
 }
