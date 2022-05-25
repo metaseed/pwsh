afdc51dee6c0f1580c30d93d7509f9f4143cd893
metasong <metaseed@gmail.com>
2022-05-25 12:42:40 -0500

    chore

--- Module/Metaseed.Utility/Write-Execute.ps1
+++ Module/Metaseed.Utility/Write-Execute.ps1
@@ -10,7 +10,8 @@ function Write-Execute {
   )
   process {
     # Write-Progress -Activity "${command}" -status "$('' -eq $message ? ' ': ": $message")" -Id 2 -ParentId 0
-    Write-Host " ―→ ${command} $('' -eq $message ? '': ": $message")" -ForegroundColor Blue
+    $icon = $env:WT_SESSION ? '-->': '―→'
+    Write-Host " $icon ${command} $('' -eq $message ? '': ": $message")" -ForegroundColor Blue
     # note: if put parenthesis around: return (iex $command), the output would be no color
     # i.e. Write-Execute 'git status', if there are modification, no red text for modification files
     return iex $command
