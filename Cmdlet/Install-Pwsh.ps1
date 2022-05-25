8959cfcc5bbeb83cdd039d3a4de388dbc28d9707
metasong <metaseed@gmail.com>
2022-05-25 15:30:27 -0500

    install-terminal

--- Cmdlet/Install-Pwsh.ps1
+++ Cmdlet/Install-Pwsh.ps1
@@ -10,17 +10,27 @@ Assert-Admin
 Breakable-Pipeline {
   Get-GithubRelease -OrgName 'Powershell' -RepoName 'PowerShell' -Version $version -fileNamePattern 'win-x64.msi$' |
   % {
+    $asset = $_
+    if ($assets.Count -ne 1 ) {
+      foreach ($asset in $assets) {
+        Write-Host $asset.name
+      }
+      Write-Error "Expected one asset, but found $assets.Count"
+      break;
+    }
+    
     $_.name -match "-(\d+\.\d+\.\d+\.*\d*)-" >$null
     $ver_online = [Version]::new($matches[1])
     $version = $PSVersionTable.PSVersion
-    if($ver_online -le $version) {
+    if ($ver_online -le $version) {
       Write-Host "You are using the latest version of PowerShell.`n $version is the latest version available."
       break
     }
     write-host "You are using PowerShell $version.`n The latest version is $ver_online."
     return $_
-  }|
-  Download-GithubRelease | % {
+  } |
+  Download-GithubRelease | 
+  % {
     Confirm-Continue "to install power shell: $_ `nwe would kill all running pwsh processes.`n"
     Start-Process msiexec.exe -ArgumentList "/package $_ /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1"
     Get-Process -Name pwsh | Stop-Process -Force
--- Cmdlet/Terminal/Install-Terminal.ps1
+++ Cmdlet/Terminal/Install-Terminal.ps1
@@ -7,58 +7,75 @@ param (
   [Parameter()]
   [string]
   [ValidateSet('preview', 'stable')]
-  $version = 'preview'
-    
+  $version = 'preview',
+  [Parameter()]
+  [switch]
+  $force
+
 )
 Assert-Admin
 
-$assets = Get-GithubRelease -OrgName 'microsoft' -RepoName 'terminal' -version $version -fileNamePattern "_$($platform)_.*\.msixbundle$" -ErrorAction Stop
+Breakable-Pipeline {
+  Get-GithubRelease -OrgName 'microsoft' -RepoName 'terminal' -version $version -fileNamePattern "_$($platform)_.*\.msixbundle$" -ErrorAction Stop |
+  %{
+    $assets = $_
+    if ($assets.Count -ne 1 ) {
+      foreach ($asset in $assets) {
+        Write-Host $asset.name
+      }
+      Write-Error "Expected one asset, but found $assets.Count"
+      break;
+    }
+    $file = $assets[0].name
+    write-host $file
+    "$file" -match "_(\d+\.\d+\.\d+\.\d+)_"
+    $ver_online = [Version]::new($matches[1]);
+    Write-substep 'checking installed version...'
+    Import-Module Appx -UseWindowsPowerShell *>$null
+    $t = Get-AppxPackage *WindowsTerminal*
+    if ($t) {
+      if ($t.Version -ge $ver_online -and !$force) {
+        Write-Warning "already latest version: $ver_online, no need to update"
+        break;
+      }
+      else {
+        write-host "new version available:  $ver_online, local: $t.Version"
+      }
+    }
+    else {
+      write-host "version to install:  $ver_online"
+    }
 
-if ($assets.Count -ne 1 ) {
-  foreach ($asset in $assets) {
-    Write-Host $asset.name
+    Write-Step 'download windows terminal...'
+    return $_
+  } |
+  Download-GithubRelease |
+  % {
+    Write-Step 'check running WindowsTerminal...'
+    $install = @"
+    & {
+      Write-Step 'install windows terminal...'
+      write-host `"Add-AppxPackage '$env:temp/$file'`"
+      Import-Module Appx -UseWindowsPowerShell *>`$null
+      Add-AppxPackage `"$env:temp/$file`"
+      Write-host `"when err: please kill the 'Terninal' task in task manager and try again with:  Add-AppxPackage '$env:temp\$file'`"
+      Setup-Terminal
+    }
+"@
+
+    $wts = get-process -name "WindowsTerminal" -ErrorAction SilentlyContinue
+    if ($wts) {
+      Confirm-Continue "Windows Terminal is running. Do you want to kill it?"
+
+      $Wt = Get-ParentProcessByName -processName 'WindowsTerminal'
+      if ($Wt) {
+        # $path = $MyInvocation.MyCommand.Path
+        Start-Process pwsh -verb runas -ArgumentList @( "-NoExit", "-Command",  $install )
+      } else {
+        pwsh -NoExit -Command $install
+      }
+      stop-process -name "WindowsTerminal" -force
+      stop-process -name "OpenConsole" -force
+    } 
   }
-  Write-Error "Expected one asset, but found $assets.Count"
-  return
 }
-
-$file = $assets[0].name
-write-host $file
-"$file" -match "_(\d+\.\d+\.\d+\.\d+)_"
-$ver_online = [Version]::new($matches[1]);
-Write-substep 'checking installed version...'
-Import-Module Appx -UseWindowsPowerShell *>$null
-$t = Get-AppxPackage *WindowsTerminal*
-if ($t) {
-  if ($t.Version -ge $ver_online) {
-    Write-Warning 'already latest version, no need to update'
-    return 1
-  }
-  else {
-    write-host "new version available:  $ver_online, local: $t.Version"
-  }
-}
-else {
-  write-host "version available:  $ver_online"
-}
-
-Write-Step 'check running WindowsTerminal...'
-$wts = get-process -name "WindowsTerminal" -ErrorAction SilentlyContinue
-if ($wts) {
-  Confirm-Continue "Windows Terminal is running. Do you want to kill it?"
-
-  $Wt = Get-ParentProcessByName -processName 'WindowsTerminal'
-  if ($Wt) {
-    $path = $MyInvocation.MyCommand.Path
-    Start-Process pwsh -verb runas -ArgumentList @( "-NoExit", "-File", "$path", "$processName" )
-  }
-  stop-process -name "WindowsTerminal" -force
-}
-
-Write-Step 'download windows terminal...'
-Download-GithubRelease $assets
-Write-Step 'install windows terminal...'
-write-host "Add-AppxPackage '$env:temp/$file'"
-Add-AppxPackage "$env:temp/$file"
-Confirm-Continue "if err:`n please kill the 'Terninal' task in task manager and try again with: `n Add-AppxPackage '$env:temp/$file'"
-Setup-Terminal
