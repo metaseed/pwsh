4d70b9c275d71e7abb22366e40d235ba08b37c09
metasong <metaseed@gmail.com>
2022-05-27 15:35:03 -0500

    ok

--- Module/Metaseed.Env/Update-Env.ps1
+++ Module/Metaseed.Env/Update-Env.ps1
@@ -9,23 +9,28 @@ function Update-Env {
    @("Machine", "User")  | 
    % { [Environment]::GetEnvironmentVariables($_).GetEnumerator() } |
    % {
-      # special handle, otherwise username would be replaced to 'System'
-      if($_.Name -eq 'UserName' -or $_.Name -eq 'TEMP' -or $_.Name -eq 'TMP') {
-         return $_
-      }
       # For Path variables, append the new values, if they're not already in there
       $envValue = Get-Content "Env:$($_.Name)" -ErrorAction SilentlyContinue
-
       if ($_.Name -match 'Path$|^PATHExt$' -and ($_.Value.Contains(';') -or $envValue.Contains(';'))) { 
          $_.Value = ("$envValue;$($_.Value)" -split ';' | Select-Object -unique) -join ';'
       }
+      $_
+   } |
+   ? {
+      # special handle, otherwise username would be replaced to 'System'
+      if ($_.Name -eq 'UserName' -or $_.Name -eq 'TEMP' -or $_.Name -eq 'TMP') {
+         return $false
+      }
+      $envValue = Get-Content "Env:$($_.Name)" -ErrorAction SilentlyContinue
       $update = $_.Value -ne $envValue
-      if($update) {
+      if ($update) {
          write-host "updated $($_.Name), from: $envValue"
          write-host "    to: $($_.Value)"
          write-host ''
+      } else {
+         return $false
       }
-      $_
+      return $true
    } |
    Set-Content -Path { "Env:$($_.Name)" }
    
