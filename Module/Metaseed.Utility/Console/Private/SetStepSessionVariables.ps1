e4ed7f0c22dcce59c5a124ce6c1bdff4daaae25a
metasong <metaseed@gmail.com>
2022-05-31 17:40:02 -0500

    ok

--- Module/Metaseed.Utility/Console/Private/SetStepSessionVariables.ps1
+++ Module/Metaseed.Utility/Console/Private/SetStepSessionVariables.ps1
@@ -1,9 +1,9 @@
-Register-EngineEvent -SourceIdentifier  'SetSessionScope' {
+Register-EngineEvent -SourceIdentifier  'SessionScopeEvent' {
     $line = $event.SourceArgs.line
     # $cursor = $event.SourceArgs.cursor
     $scope = $event.SourceArgs.scope
     $lastScope = $event.SourceArgs.lastScope
-    # Write-host "SetSessionScope: $scope, $line, $cursor"
+    # Write-host "SessionScopeEvent: $scope, $line, $cursor"
     $scope.Steps  = $lastScope.Steps
     $scope.lazyStepsInit = $true
     # if ($line -match '^\s*(Show-Steps|ss$)') {
