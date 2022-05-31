0d6a4108b079f955b08925b3b95e71db3a2bfca0
metasong <metaseed@gmail.com>
2022-05-31 14:02:42 -0500

    ok

--- Module/Metaseed.Git/Git-HasLocalChanges.ps1
+++ Module/Metaseed.Git/Git-HasLocalChanges.ps1
@@ -2,7 +2,7 @@ function Git-HasLocalChanges {
   [CmdletBinding()]
   param ()
   $status = git status
-  $dirtyMsg = $status -match 'modified:|Untracked files:|Your branch is ahead of'
+  $dirtyMsg = $status -match 'modified:|Untracked files:|Your branch is ahead of| have diverged'
   if ($dirtyMsg.length -eq 0) {
     Write-Host "Your do not have local changes`n$status"
     return $false
