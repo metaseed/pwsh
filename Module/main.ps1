b802a715f08be1a600b66f2db4675bca6da6249a
metasong <metaseed@gmail.com>
2022-06-01 15:01:09 -0500

    ok

--- Module/main.ps1
+++ Module/main.ps1
@@ -1,8 +1,10 @@
 # list the module that need to exposed by default
 # using module Metaseed.Env
+# remove warning in console of none standard verb in command
+# remove waring:  include unapproved verbs that might make them less discoverable. 
 Import-Module Metaseed.Lib -DisableNameChecking
 Import-Module Metaseed.Env -DisableNameChecking
-Import-Module Metaseed.Utility -DisableNameChecking # remove waring:  include unapproved verbs that might make them less discoverable. 
-Import-Module Metaseed.Git -DisableNameChecking # remove warning in console of none standard verb in command
+Import-Module Metaseed.Utility -DisableNameChecking 
+Import-Module Metaseed.Git -DisableNameChecking 
 Import-Module Metaseed.Sound -DisableNameChecking
 
