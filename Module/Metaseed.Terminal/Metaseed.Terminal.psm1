8b42b001abbdd33135a68f08ae8d50f52bbf3193
metasong <metaseed@gmail.com>
2022-06-16 17:42:29 -0500

    ok

--- Module/Metaseed.Terminal/Metaseed.Terminal.psm1
+++ Module/Metaseed.Terminal/Metaseed.Terminal.psm1
@@ -1,6 +1,5 @@
-# Import-Module $PSScriptRoot/_bin/TerminalBackground.dll
 Initialize-WTBgImg
-# could not export any function via Export-ModuleMember, so put it psd1
+# could not export any function via Export-ModuleMember when have nested module, so put it psd1
 # . $env:MS_PWSH/Lib/Export-Functions.ps1
 # . Export-Functions $PSScriptRoot
 . $PSScriptRoot/Terminal/Play-WTBGGif.ps1
