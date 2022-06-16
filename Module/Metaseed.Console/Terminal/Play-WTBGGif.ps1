95e98c1a771ed2867fea3911c3d289004f350b35
metasong <metaseed@gmail.com>
2022-06-16 16:38:44 -0500

    ok

--- Module/Metaseed.Console/Terminal/Play-WTBGGif.ps1
+++ Module/Metaseed.Console/Terminal/Play-WTBGGif.ps1
@@ -1,3 +1,19 @@
+function GetGifs {
+  [CmdletBinding()]
+  param (
+    [Parameter()]
+    $wordToComplete
+  )
+  $gifs = @(Get-ChildItem "$psscriptroot\gifs\*.gif" | % { $_.BaseName})|
+  ? { 
+    if($wordToComplete) {
+      return $_ -like ($wordToComplete -split '' -join '*').TrimStart('*')
+    } else {
+      return $_
+    }
+  }
+  return $gifs
+}
 function Play-WTBGGif {
     param(
       [Parameter( Position=0)]
@@ -8,22 +24,7 @@ function Play-WTBGGif {
     $str =  '{"backgroundImage": ' +  (ConvertTo-Json "$psscriptroot\gifs\$name.gif") + ',"backgroundImageStretchMode": "none","backgroundImageAlignment": "center","backgroundImageOpacity":0.8}'
     Set-WTBgImg defaults $durationInseconds $str
 }
-function GetGifs {
-  [CmdletBinding()]
-  param (
-    [Parameter()]
-    $wordToComplete
-  )
-  $gifs = @(Get-ChildItem "$psscriptroot\gifs\*.gif" | % { $_.BaseName})
-  ? { 
-    if($wordToComplete) {
-      return $_ -like ($wordToComplete -split '' -join '*').TrimStart('*')
-    } else {
-      return $_
-    }
-  }
-  return $gifs
-}
+
 
 #Note: directly use ArgumentCompleter and call GetWindowsSounds in it's script block not works!
 Register-ArgumentCompleter -CommandName 'Play-WTBGGif' -ParameterName 'name' -ScriptBlock {
