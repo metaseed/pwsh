2ceeab0a234b22181746c47f3935e3535d32e661
metasong <metaseed@gmail.com>
2022-07-24 17:24:35 -0500

    ok

--- Cmdlet/s/Command/update-Exports.ps1
+++ Cmdlet/s/Command/update-Exports.ps1
@@ -1,9 +1,11 @@
 [CmdletBinding()]
 param (
-    [Parameter()]
-    [string]
-    $module = 'metaseed.management'
+  [Parameter()]
+  [string]
+  $module = 'metaseed.terminal'
 )
+
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass # can not load zlocatin.psm1
 
 $mo = ipmo $module -PassThru -DisableNameChecking
 $path = (split-path $mo.Path) + "\$module.psd1"
@@ -11,11 +13,24 @@ $path = (split-path $mo.Path) + "\$module.psd1"
 # $psd = (gc $path -raw | Invoke-Expression)
 # $psd.ExportedFunctions = $mo.ExportedFunctions
 # [System.Management.Automation.PSSerializer]::Serialize($psd) | Out-File $path
-$func = $mo.ExportedFunctions.Keys -join "','"
 $content = gc $path -raw
-if($content -notlike '*FunctionsToExport*') {
-  $newContent = $content  -replace "ModuleVersion.+`n", "`$0  FunctionsToExport = @('$func')`n" 
-} else{
-  $newContent = $content  -replace "(FunctionsToExport\s*=\s*@\().+\)", "`$1'$func')" 
+$func = $mo.ExportedFunctions.Keys -join "','"
+Write-Verbose $func
+if ($content -notlike '*FunctionsToExport*') {
+  $content = $content -replace "ModuleVersion.+`n", "`$0  FunctionsToExport = @('$func')`n" 
 }
-$newContent | out-file $path
+else {
+  $content = $content -replace "(FunctionsToExport\s*=\s*@\().+\)", "`$1'$func')" 
+}
+#   $content = $content -replace "FunctionsToExport\s*=\s*@\(.*\)", ''
+$cmd = $mo.ExportedCmdlets.Keys -join "','"
+Write-Verbose $cmd
+
+if ($content -notlike '*CmdletsToExport*') {
+  $content = $content -replace "ModuleVersion.+`n", "`$0  CmdletsToExport = @('$cmd')`n" 
+}
+else {
+  $content = $content -replace "(CmdletsToExport\s*=\s*@\().+\)", "`$1'$cmd')" 
+}
+# $content = $content -replace "CmdletsToExport\s*=\s*@\(.*\)", ''
+$content | out-file $path
