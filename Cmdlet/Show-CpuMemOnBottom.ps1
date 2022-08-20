48c42459292022d1eb0affc096c1ff62a4f2eeb2
metasong <metaseed@gmail.com>
2022-08-19 19:05:45 -0500

    ok

--- Cmdlet/Show-CpuMemOnBottom.ps1
+++ Cmdlet/Show-CpuMemOnBottom.ps1
@@ -28,19 +28,18 @@ Start-ThreadJob -ScriptBlock {
         [Console]::Write($text)
         [Console]::SetCursorPosition($x, $y)
     }
-    $totalRam = (Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).Sum
 
+    $totalRam = (Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).Sum
     while ($true) {
-        $usedMem = (((Get-Ciminstance Win32_OperatingSystem).TotalVisibleMemorySize * 1kb) - ((Get-Counter -Counter "\Memory\Available Bytes").CounterSamples.CookedValue))
+        $usedMem = (((Get-Ciminstance Win32_OperatingSystem).TotalVisibleMemorySize * 1kb) - ((Get-Counter -Counter "\Memory\Available Bytes").CounterSamples.CookedValue)) / 1Mb
         
         $cpuTime = (Get-Counter '\Processor(_Total)\% Processor Time').CounterSamples.CookedValue
         $cpuPer = "$($cpuTime.ToString("#,0.0"))".PadLeft(3) + '%'
-        $availMem = (Get-Counter '\Memory\Available MBytes').CounterSamples.CookedValue
-        $memPer = (104857600 * $availMem / $totalRam).ToString("#,0.0").PadLeft(3) + '%'
+        # $availMem = (Get-Counter '\Memory\Available MBytes').CounterSamples.CookedValue
+        $memPer = (104857600 * $usedMem / $totalRam).ToString("#,0.0").PadLeft(3) + '%'
 
-        $memPer = 
         $cpu = "CPU: $cpuPer"
-        $mem = "Mem: $(($availMem / 1KB).ToString("#,0.0"))GB($memPer)"
+        $mem = "Mem: $(($usedMem / 1KB).ToString("#,0.0"))GB($memPer)"
         $str = "$cpu | $mem"
         $maxLen = [Math]::Max($lastLen, $str.Length)
         $lastLen = $str.Length
