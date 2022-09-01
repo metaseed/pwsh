1da022c7ee4db2c11ddf8081edb175f3e4278e9d
metasong <metaseed@gmail.com>
2022-09-01 10:46:26 -0500

    parentSquare attachment download

--- js/parentSquare-attachment-downloadAll.js
+++ js/parentSquare-attachment-downloadAll.js
@@ -2,7 +2,8 @@
 // @name         New Userscript
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  download all attachement from ParentSquare, if the 'download all' button is not there, run this script.
+// @description  download all attachments from ParentSquare, if the 'download all' button is not there, it will create a 'download all' button for you.
+//               this script could be used in tampermonkey, or just run this script in the browser console.
 // @author       metasong
 // @match        https://www.parentsquare.com/feeds/*
 // @icon         https://www.google.com/s2/favicons?domain=parentsquare.com
@@ -10,6 +11,14 @@
 // ==/UserScript==
 
 (function () {
+  var all = document.body.querySelector('a[aria-label="Download All"]')
+  if(all) {
+    var msg = 'there already is a "Download All" button, please use it!'
+    console.log(msg)
+    alert(msg)
+    return
+  }
+
   console.log('click "download all" to download...');
   ("use strict");
   function createButton(title, action) {
@@ -19,8 +28,8 @@
     d.setAttribute("role", "button");
     d.addEventListener("click", action, false);
     // d.textContent = '🪂 Download All'
-    d.innerHTML = `<i class="fa fa-download fa-fw color-highlight"></i> 
-    Download All`;
+    d.innerHTML = `<i class="fa fa-download fa-fw color-highlight"></i>
+    Download All🪂`;
     return d;
   }
 
@@ -31,7 +40,7 @@
   }
 
   let butt = createButton(
-    'download with this link if no "Download All"',
+    'download with this link if no "Download All" button',
     downloadAllPic
   );
   let nav = document.body.querySelector(".nav-stacked");
