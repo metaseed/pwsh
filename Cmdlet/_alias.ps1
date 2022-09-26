0cd34fbbc792bd8d19cf1d0531c3d5f172184507
metasong <metaseed@gmail.com>
2022-09-26 17:56:43 -0500

    folder

--- Cmdlet/_alias.ps1
+++ Cmdlet/_alias.ps1
@@ -1,7 +1,5 @@
 #Here, Set-Alias is better than New-Alias, because if there is already one with same name, it would not throw but replace it
-Set-Alias weather Show-Weather
 Set-Alias cpu Show-CpuUsage
-Set-Alias space Show-DiskSpace
 Set-Alias bumpVer Update-InfoVersion
 Set-Alias sandbox Open-Sandbox
 function __change_directory{
