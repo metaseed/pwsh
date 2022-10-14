2541d5258fbe2ce2aaa973b82dc8a2490c34bc9b
metasong <metaseed@gmail.com>
2022-10-14 09:53:38 -0500

    install

--- install.ps1
+++ install.ps1
@@ -15,7 +15,7 @@ if($Version -eq 'latest') {
     $url = "https://github.com/metasong/pwsh/archive/refs/tags/$Version.zip" #1.0.2
 }
 iwr $url -OutFile $zip
-# 
+#
 # ~ is better than $env:HomePath, it include the home drive, just like $home
 Expand-Archive $zip ~/metaseed -Force
 
@@ -23,9 +23,9 @@ Expand-Archive $zip ~/metaseed -Force
 # mi works even file is used
 # but mi would show error if des exists: because a file or directory with the same name already exists.
 # the .ms_pwsh-del shouldn't be used/locked by any app
-ri ~/metaseed/.ms_pwsh-del -Force -Recurse -ErrorAction SilentlyContinue
-mi ~/metaseed/ms_pwsh ~/metaseed/.ms_pwsh-del -Force -ErrorAction SilentlyContinue
-ri ~/metaseed/.ms_pwsh-del -Force -Recurse -ErrorAction SilentlyContinue
+ri '~/metaseed/.ms_pwsh-del' -Force -Recurse -ErrorAction SilentlyContinue
+mi '~/metaseed/ms_pwsh' '~/metaseed/.ms_pwsh-del' -Force -ErrorAction SilentlyContinue
+ri '~/metaseed/.ms_pwsh-del' -Force -Recurse -ErrorAction SilentlyContinue
 ri $zip -Force
 
-. ~/metaseed/ms_pwsh/config.ps1
+. '~/metaseed/ms_pwsh/config.ps1'
