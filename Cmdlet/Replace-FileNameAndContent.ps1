d0643506d378b82c37d52cb148e0676641f05c34
metasong <metaseed@gmail.com>
2022-10-28 16:05:04 -0500

    ok

--- Cmdlet/Replace-FileNameAndContent.ps1
+++ Cmdlet/Replace-FileNameAndContent.ps1
@@ -13,20 +13,36 @@ param (
     # extensions to filter
     [Parameter()]
     [string[]]
-    $exts = ('*.cs', '*.csproj', '*.sln', '*.resx', '*.targets', '*.xml', '*.config', '*.xaml')
-)
+    $exts = @('*.cs', '*.csproj', '*.sln', '*.resx', '*.targets', '*.xml', '*.config', '*.xaml', '*.json', '*.md', '*.ps1'),
 
+    [Parameter()]
+    [string[]]
+    $excludeDirs = '\\bin\\|\\obj\\',
+
+    [switch]$noContent
+
+)
+# git reset --hard origin/master
+# git clean -f -d
 ## rename file names
 $dir = get-location
-$files = Get-ChildItem $dir -filter "*$match*" -Recurse
+$files = Get-ChildItem $dir  -Recurse -filter "*$match*" | ? { $_.FullName -notmatch $excludeDirs }
 
 $files |
 Sort-Object -Descending -Property FullName |
-Rename-Item -newname { $_.name -replace $match, $replacement } -force
+# | select FullName
+Rename-Item -force -newname {
+    write-host "rename: $($_.FullName)";
+    return $_.name -replace $match, $replacement
+}
 
-## rename file contents
-Get-ChildItem $dir -include $exts -Recurse |
-% { Get-Content $_.fullname -Encoding UTF8 |
-    % { $_ -creplace $match, $replacement } |
-    set-content $_.fullname -Encoding UTF8 
+if (!$noContent) {
+    ## replace file contents
+    Get-ChildItem $dir -include $exts -Recurse |
+    ? { $_.FullName -notmatch $excludeDirs } |
+    % {
+    (Get-Content $_.fullname -Encoding UTF8 -Raw) |
+        % { $_ -creplace $match, $replacement } |
+        set-content $_.fullname -Encoding UTF8 -Force -NoNewline
+    }
 }
--- Profile/configPsFzf.ps1
+++ Profile/configPsFzf.ps1
@@ -1,2 +1,2 @@
-
+# https://github.dev/kelleyma49/PSFzf
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
