33ccc8bb647476a4bdca590f00bf71d036ce2e75
metasong <metaseed@gmail.com>
2022-11-02 14:31:40 -0500

    ok

--- Cmdlet/install/install-python.ps1
+++ Cmdlet/install/install-python.ps1
@@ -9,7 +9,16 @@ if(gcm winget -ErrorAction Ignore) {
   return
 }
 
-iwr https://www.python.org/ftp/python/3.10.5/python-3.10.5-amd64.exe -OutFile $env:temp\python3.exe
+$resp = iwr https://www.python.org/downloads/windows/
+$content = $resp.content
+$content -match 'href="(.+)">Latest Python' > $null
+$part = $matches[1]
+$resp = iwr "https://www.python.org$part"
+$content = $resp.content
+$content -match 'href="(.+)">Windows installer \(64-bit\)' > $null
+$url = $matches[1]
+# https://www.python.org/ftp/python/3.10.5/python-3.10.5-amd64.exe
+iwr $url -OutFile $env:temp\python3.exe
 # & $env:temp\python3.exe /h
 # passive to prevent user customization
 & $env:temp\python3.exe /passive
