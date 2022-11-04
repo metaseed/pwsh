9c575267027f9eac7a59477780c9269ff3238f03
metasong <metaseed@gmail.com>
2022-11-04 12:48:41 -0500

    ok

---
+++ Module/Metaseed.Management/Admin/Test-ProcessElevated.ps1
@@ -0,0 +1,6 @@
+function Test-ProcessElevated {
+  # https://stackoverflow.com/questions/1220213/detect-if-running-as-administrator-with-or-without-elevated-privileges/17492949#answer-1220234
+  # https://github.com/falahati/UACHelper
+  # https://www.nuget.org/packages/UACHelper/
+
+}
