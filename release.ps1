df6e2d91faeff038fd09b75a8a2cf87d0bed7345
metasong <metaseed@gmail.com>
2023-06-26 12:06:28 -0500

    release 1.2.8

--- release.ps1
+++ release.ps1
@@ -1,3 +1,4 @@
+#
 [CmdletBinding()]
 param (
   [Parameter()]
@@ -5,24 +6,28 @@ param (
   [ValidateSet('major', 'minor', 'build')]
   $Bump = 'build'
 )
-
+# modify the version string
 $info = (Get-Content "$PSScriptRoot\info.json") | ConvertFrom-Json | % {
   $version = [Version] $_.version
   $major = $version.Major
   $minor = $version.Minor
   $build = $version.Build
+
   if ($Bump -eq 'major') { $major = $version.Major + 1 }
   elseif ($Bump -eq 'minor') { $minor = $version.Minor + 1 }
   else { $build = $version.Build + 1 }
+
   $_.version = "$major.$minor.$build"
   $_
 }
+
+# write back to info.json
 $info|
 ConvertTo-Json|
 Set-Content "$PSScriptRoot\info.json"
 
+# add tag release
 $msg = "release $($info.version)"
-
 git add -A
 git commit -am $msg
 git push
