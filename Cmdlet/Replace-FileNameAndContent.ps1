2139a02a06f722771f433cc27eeee8339967f240
metasong <metaseed@gmail.com>
2024-02-27 16:07:59 +0100

    replace-fileDirNameAndContent

--- Cmdlet/Replace-FileNameAndContent.ps1
+++ Cmdlet/Replace-FileNameAndContent.ps1
@@ -28,42 +28,48 @@ param (
 
 $matches |
 % {
-    Write-Host "================================================================"
+    Write-Step "Replace File/Dir Names"
     Write-Host "handling keyword: $_"
     $match = $_
     # git reset --hard origin/master
     # git clean -f -d
     ## rename file names
     $dir = get-location
-    $files = Get-ChildItem $dir  -Recurse -filter "*$match*" | ? { $_.FullName -notmatch $excludeDirs }
+    # not case sensitive
+    # $files = Get-ChildItem $dir  -Recurse -filter "*$match*" | ? { $_.FullName -notmatch $excludeDirs }
+    $sortedFiles = Get-ChildItem $dir  -Recurse | ? { $_.Name -cmatch $match -and $_.FullName -notmatch $excludeDirs }|
+    # file processed first
+    Sort-Object -Descending -Property FullName
 
-    $files |
-    Sort-Object -Descending -Property FullName |
+    # create a temp var $sotredFiles before pipe the files to rename to avoid the error:
+    # Access to the path 'file path' is denied.
+    # which means we processed all items and buffer it the a list pointed by the temp var
+    $sortedFiles |
     # | select FullName
     Rename-Item -force -newname {
-        $newName = $_.name -replace $match, $replacement
-        write-host "rename: $($_.FullName) to: $newName";
+        $newName = $_.name -creplace $match, $replacement
+        Write-Notice "rename: $($_.FullName) to: $newName";
         return $newName
     }
 
-    if (!$noContent) {
-        Write-Host "Replace File Contents"
-        Get-ChildItem $dir -File -Recurse -include $exts |
-        ? { $_.FullName -notmatch $excludeDirs } |
-        % {
-            ## replace file contents
-            # () is important here: The process cannot access the file, because it is being used by another process.
-            $name = $_.FullName
+    if ($noContent) { return }
+
+    Write-Step "Replace File Contents"
+    Get-ChildItem $dir -File -Recurse -include $exts |
+    ? { $_.FullName -notmatch $excludeDirs } |
+    % {
+        ## replace file contents
+        # () is important here: The process cannot access the file, because it is being used by another process.
+        $name = $_.FullName
         (Get-Content $_ -Encoding utf8 -Raw) |
-            % {
-                if ($_ -match $match) {
-                    Write-Host "Replace file content: $name"
-                    return $_ -creplace $match, $replacement
-                }
-                return $_
-            } |
-            set-content $_.fullname -Encoding UTF8 -Force -NoNewline
-        }
+        % {
+            if ($_ -match $match) {
+                Write-Notice "Replace file content: $name"
+                return $_ -creplace $match, $replacement
+            }
+            return $_
+        } |
+        set-content $_.fullname -Encoding UTF8 -Force -NoNewline
     }
 }
 ## usage example: create a project Modbus from a ToDo template project
@@ -75,7 +81,7 @@ $NewKey = 'Opcua' #'Modbus'
 Replace-FileNameAndContent -match $Key -replace $NewKey -noContent
 git add .
 git commit -am "rename file and directory"
-# note the a in the -am is 'all', it just add modified and deleted files not include added
+# note the a in the -am is 'all', otherwise it just add modified and deleted files not include added
 Replace-FileNameAndContent -match $Key -replace $NewKey
 git add .
 git commit -am "rename file content"
