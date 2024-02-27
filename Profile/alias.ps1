627fdff08461b0939f3ac837957701876e4bd0db
metasong <metaseed@gmail.com>
2024-02-27 13:47:48 +0100

    update alias

--- Profile/alias.ps1
+++ Profile/alias.ps1
@@ -1,7 +1,7 @@
 Set-Alias ga Get-Alias
 Set-Alias ghp Get-Help # gh collide with gh.exe github cli
 Set-Alias ca Compress-Archive
-
+Set-Alias sa Start-Process
 . $PSScriptRoot\..\Cmdlet\_alias.ps1
 
 & { # prevent $codePath leak in to profile variable: provider
