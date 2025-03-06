956d5fb68c9e4e5c065aaeff727f8c04ff8221d0
metasong <metaseed@gmail.com>
2025-03-06 15:30:11 +0100

    improvement

--- Cmdlet/install/install-PdfSam.ps1
+++ Cmdlet/install/install-PdfSam.ps1
@@ -5,5 +5,60 @@ param (
 
 # to reload and debug after modification of lib functions
 # ipmo Metaseed.Management -Force
+function Split-Parameters {
+  [CmdletBinding()]
+  param (
+      [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
+      [AllowEmptyCollection()]
+      [object[]]$InputParameters
+  )
 
-Install-FromGithub https://github.com/torakiki/pdfsam '-windows\.zip$' -versionType 'stable' @Remaining
+  begin {
+      $PositionalParams = @()
+      $NamedParams = @{}
+  }
+
+  process {
+      # Process the arguments
+      for ($i = 0; $i -lt $InputParameters.Count; $i++) {
+          $current = $InputParameters[$i]
+
+          # Check if it's a parameter name (starts with - or has an = sign)
+          if ($current -is [string] -and ($current -match "^-" -or $current -match "=")) {
+              # It's a named parameter
+              if ($current -match "^-(\w+)=(.*)$") {
+                  # Handle -param=value format
+                  $NamedParams[$matches[1]] = $matches[2]
+              }
+              elseif ($current -match "^-(\w+)$" -and ($i + 1) -lt $InputParameters.Count -and
+                     !($InputParameters[$i + 1] -is [string] -and $InputParameters[$i + 1] -match "^-")) {
+                  # Handle -param value format
+                  $NamedParams[$matches[1]] = $InputParameters[$i + 1]
+                  $i++ # Skip the next item as we've used it as a value
+              }
+              else {
+                  # It's a switch parameter
+                  $paramName = $current.TrimStart('-')
+                  $NamedParams[$paramName] = $true
+              }
+          }
+          else {
+              # It's a positional parameter
+              $PositionalParams += $current
+          }
+      }
+  }
+
+  end {
+      # Return a custom object with both parameter collections
+      return [PSCustomObject]@{
+          PositionalParameters = $PositionalParams
+          NamedParameters = $NamedParams
+      }
+  }
+}
+
+$param = Split-Parameters $Remaining
+$pos =$param.PositionalParameters
+$nam = $param.NamedParameters
+Install-FromGithub https://github.com/torakiki/pdfsam '-windows\.zip$'  @pos @nam
