c296a5b3defb6285ce9e6dbf431b6385692231e4
metasong <metaseed@gmail.com>
2026-01-16 09:29:27 +0100

    refactor

--- Module/Metaseed.MetaGo/Metaseed.MetaGo.psm1
+++ Module/Metaseed.MetaGo/Metaseed.MetaGo.psm1
@@ -1,31 +1,15 @@
 using namespace System.Management.Automation
 using namespace System.Collections.Generic
 
-function Invoke-MetaJump {
-    [CmdletBinding()]
-    param()
-
-    # 1. Get Current Buffer State
+function Get-BufferInfo {
     $line = $null
-    $initialCursor = $null
-    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$initialCursor)
-    # 2. Prompt for target character
-    # We don't want to mess up the screen yet.
-    # A simple way is to show a small indicator or just wait.
-    # Let's write a small prompt below the current line or just wait.
-
-    # Getting the start position of the input on screen is tricky due to prompt length.
-    # We can infer it: CurrentConsolePosition - BufferCursorPosition
+    $cursor = $null
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+    
     $consoleLeft = [Console]::CursorLeft
     $consoleTop = [Console]::CursorTop
-
-    # Calculate where the input starts (assuming single line prompt for simplicity initially,
-    # but robust logic should handle wrapping)
-    # This is a naive calculation for the "Start of Input"
-    # It might be negative if wrapping occurred.
-    # For a robust solution, we might just redraw the whole line from the calculated start.
-
-    $startLeft = $consoleLeft - $initialCursor
+    
+    $startLeft = $consoleLeft - $cursor
     $startTop = $consoleTop
 
     while ($startLeft -lt 0) {
@@ -33,117 +17,191 @@ function Invoke-MetaJump {
         $startTop--
     }
 
-    # Hide cursor during selection
+    return @{
+        Line = $line
+        Cursor = $cursor
+        ConsoleLeft = $consoleLeft
+        ConsoleTop = $consoleTop
+        StartLeft = $startLeft
+        StartTop = $startTop
+    }
+}
+
+function Show-Tooltip {
+    param($Top)
+    
+    $tooltip = "please type the char to jump to..."
+    
+    if ($Top -lt [Console]::BufferHeight) {
+        [Console]::SetCursorPosition(0, $Top)
+        $originalColor = [Console]::ForegroundColor
+        [Console]::ForegroundColor = 'Cyan'
+        [Console]::Write($tooltip)
+        [Console]::ForegroundColor = $originalColor
+    }
+    return $tooltip.Length
+}
+
+function Clear-Tooltip {
+    param($Top, $Length)
+    
+    if ($Top -lt [Console]::BufferHeight) {
+        [Console]::SetCursorPosition(0, $Top)
+        [Console]::Write(" " * $Length)
+    }
+}
+
+function Show-Indicator {
+    param($Info)
+    
+    [Console]::SetCursorPosition($Info.ConsoleLeft, $Info.ConsoleTop)
+    $indicatorChar = ' '
+    if ($Info.Cursor -lt $Info.Line.Length) {
+        $indicatorChar = $Info.Line[$Info.Cursor]
+    }
+    
+    $prevBg = [Console]::BackgroundColor
+    $prevFg = [Console]::ForegroundColor
+    
+    [Console]::BackgroundColor = 'Green'
+    [Console]::ForegroundColor = 'Black'
+    [Console]::Write($indicatorChar)
+    
+    [Console]::BackgroundColor = $prevBg
+    [Console]::ForegroundColor = $prevFg
+    [Console]::SetCursorPosition($Info.ConsoleLeft, $Info.ConsoleTop)
+    
+    return @{ Char = $indicatorChar; Bg = $prevBg; Fg = $prevFg }
+}
+
+function Restore-Indicator {
+    param($Info, $SavedState)
+    
+    [Console]::SetCursorPosition($Info.ConsoleLeft, $Info.ConsoleTop)
+    [Console]::BackgroundColor = $SavedState.Bg
+    [Console]::ForegroundColor = $SavedState.Fg
+    [Console]::Write($SavedState.Char)
+    [Console]::SetCursorPosition($Info.ConsoleLeft, $Info.ConsoleTop)
+}
+
+function Get-CharMatches {
+    param($Line, $Char)
+    
+    $matches = @()
+    for ($i = 0; $i -lt $Line.Length; $i++) {
+        if ($Line[$i] -eq $Char) {
+            $matches += $i
+        }
+    }
+    return $matches
+}
+
+function Get-ColorMap {
+    return @(
+        @{ Key = 'g'; Color = "42"; Label = "Green" }
+        @{ Key = 'w'; Color = "47"; Label = "White" }
+        @{ Key = 'r'; Color = "41"; Label = "Red" }
+        @{ Key = 'b'; Color = "44"; Label = "Blue" }
+        @{ Key = 'c'; Color = "46"; Label = "Cyan" }
+        @{ Key = 'y'; Color = "43"; Label = "Yellow" }
+        @{ Key = 'm'; Color = "45"; Label = "Magenta" }
+    )
+}
+
+function Draw-Overlay {
+    param($Info, $Matches, $ColorMap)
+    
+    $overlayParts = @()
+    $lastIndex = 0
+    $matchIndex = 0
+
+    foreach ($idx in $Matches) {
+        if ($idx -gt $lastIndex) {
+            $overlayParts += $Info.Line.Substring($lastIndex, $idx - $lastIndex)
+        }
+        
+        if ($matchIndex -lt $ColorMap.Count) {
+            $map = $ColorMap[$matchIndex]
+            $esc = [char]0x1b
+            $coloredChar = "$esc[$($map.Color)m$($Info.Line[$idx])$esc[0m"
+            $overlayParts += $coloredChar
+        } else {
+            $overlayParts += $Info.Line[$idx]
+        }
+        
+        $lastIndex = $idx + 1
+        $matchIndex++
+    }
+    
+    if ($lastIndex -lt $Info.Line.Length) {
+        $overlayParts += $Info.Line.Substring($lastIndex)
+    }
+
+    $overlayString = $overlayParts -join ""
+    
+    [Console]::SetCursorPosition($Info.StartLeft, $Info.StartTop)
+    [Console]::Write($overlayString)
+}
+
+function Restore-Visuals {
+    param($Info)
+    
+    # Force PSReadLine to refresh. 
+    # Logic requires cursor to be at the correct calculated physical position for the current buffer cursor.
+    
+    $line = $null
+    $cursor = $null
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+
+    $finalLeft = $Info.StartLeft + $cursor
+    $finalTop = $Info.StartTop
+    
+    while ($finalLeft -ge [Console]::BufferWidth) {
+        $finalLeft -= [Console]::BufferWidth
+        $finalTop++
+    }
+
+    [Console]::SetCursorPosition($finalLeft, $finalTop)
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert("")
+}
+
+function Invoke-MetaJump {
+    [CmdletBinding()]
+    param()
+
+    $info = Get-BufferInfo
     $cursorVisible = [Console]::CursorVisible
     [Console]::CursorVisible = $false
     
     try {
-        # --- 1. Visual Cues (Tooltip & Indicator) ---
-
-        # A. Tooltip on next line
-        $tooltip = "please type the char to jump to..."
-        $tooltipTop = $consoleTop + 1
+        # 1. Show Visual Cues
+        $tooltipTop = $info.ConsoleTop + 1
+        $tooltipLen = Show-Tooltip -Top $tooltipTop
+        $indicatorState = Show-Indicator -Info $info
         
-        # Only show if within buffer height (simple check)
-        if ($tooltipTop -lt [Console]::BufferHeight) {
-            [Console]::SetCursorPosition(0, $tooltipTop)
-            $originalColor = [Console]::ForegroundColor
-            [Console]::ForegroundColor = 'Cyan'
-            [Console]::Write($tooltip)
-            [Console]::ForegroundColor = $originalColor
-        }
-
-        # B. Indicator at current cursor (Green background)
-        [Console]::SetCursorPosition($consoleLeft, $consoleTop)
-        $indicatorChar = ' '
-        if ($initialCursor -lt $line.Length) {
-            $indicatorChar = $line[$initialCursor]
-        }
-        $prevBg = [Console]::BackgroundColor
-        $prevFg = [Console]::ForegroundColor
-        [Console]::BackgroundColor = 'Green'
-        [Console]::ForegroundColor = 'Black'
-        [Console]::Write($indicatorChar)
-        [Console]::BackgroundColor = $prevBg
-        [Console]::ForegroundColor = $prevFg
-        [Console]::SetCursorPosition($consoleLeft, $consoleTop)
-
-        # --- 2. Read Target Char ---
+        # 2. Read Target
         $targetKey = [Console]::ReadKey($true)
         
-        # --- 3. Cleanup Immediate Visuals ---
-        # Clear Tooltip
-        if ($tooltipTop -lt [Console]::BufferHeight) {
-            [Console]::SetCursorPosition(0, $tooltipTop)
-            [Console]::Write(" " * $tooltip.Length)
-        }
-        
-        # Restore Indicator (remove Green highlight)
-        [Console]::SetCursorPosition($consoleLeft, $consoleTop)
-        [Console]::BackgroundColor = $prevBg
-        [Console]::ForegroundColor = $prevFg
-        [Console]::Write($indicatorChar)
-        [Console]::SetCursorPosition($consoleLeft, $consoleTop)
+        # 3. Cleanup Immediate Visuals
+        Clear-Tooltip -Top $tooltipTop -Length $tooltipLen
+        Restore-Indicator -Info $info -SavedState $indicatorState
         
         if ($targetKey.Key -eq 'Escape') { return }
         
         $targetChar = $targetKey.KeyChar
         
-        # --- 4. Find Occurrences ---
-        $matches = @()
-        for ($i = 0; $i -lt $line.Length; $i++) {
-            if ($line[$i] -eq $targetChar) {
-                $matches += $i
-            }
-        }
-
+        # 4. Find Matches
+        $matches = Get-CharMatches -Line $info.Line -Char $targetChar
         if ($matches.Count -eq 0) { return }
-
-        # --- 5. Assign Colors ---
-        $colorMap = @(
-            @{ Key = 'g'; Color = "42"; Label = "Green" }
-            @{ Key = 'w'; Color = "47"; Label = "White" }
-            @{ Key = 'r'; Color = "41"; Label = "Red" }
-            @{ Key = 'b'; Color = "44"; Label = "Blue" }
-            @{ Key = 'c'; Color = "46"; Label = "Cyan" }
-            @{ Key = 'y'; Color = "43"; Label = "Yellow" }
-            @{ Key = 'm'; Color = "45"; Label = "Magenta" }
-        )
-
-        # --- 6. Construct Overlay ---
-        $overlayParts = @()
-        $lastIndex = 0
-        $matchIndex = 0
-
-        foreach ($idx in $matches) {
-            if ($idx -gt $lastIndex) {
-                $overlayParts += $line.Substring($lastIndex, $idx - $lastIndex)
-            }
-            if ($matchIndex -lt $colorMap.Count) {
-                $map = $colorMap[$matchIndex]
-                $esc = [char]0x1b
-                $coloredChar = "$esc[$($map.Color)m$($line[$idx])$esc[0m"
-                $overlayParts += $coloredChar
-            } else {
-                $overlayParts += $line[$idx]
-            }
-            $lastIndex = $idx + 1
-            $matchIndex++
-        }
-        if ($lastIndex -lt $line.Length) {
-            $overlayParts += $line.Substring($lastIndex)
-        }
-        $overlayString = $overlayParts -join ""
-
-        # --- 7. Draw Overlay ---
-        [Console]::SetCursorPosition($startLeft, $startTop)
-        [Console]::Write($overlayString)
-
-        # --- 8. Wait for Selection ---
+        
+        # 5. Draw Overlay
+        $colorMap = Get-ColorMap
+        Draw-Overlay -Info $info -Matches $matches -ColorMap $colorMap
+        
+        # 6. Select Jump Target
         $selectionKey = [Console]::ReadKey($true)
-
         if ($selectionKey.Key -eq 'Escape') { return }
-
+        
         $selectedMatchIndex = -1
         for ($k = 0; $k -lt $colorMap.Count; $k++) {
             if ("$($colorMap[$k].Key)" -eq "$($selectionKey.KeyChar)") {
@@ -158,23 +216,8 @@ function Invoke-MetaJump {
         }
     }
     finally {
-        # --- 9. Restore Visuals (Global Refresh) ---
         [Console]::CursorVisible = $cursorVisible
-
-        # Force PSReadLine refresh
-        $line = $null
-        $currentCursor = $null
-        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$currentCursor)
-
-        $finalLeft = $startLeft + $currentCursor
-        $finalTop = $startTop
-        while ($finalLeft -ge [Console]::BufferWidth) {
-            $finalLeft -= [Console]::BufferWidth
-            $finalTop++
-        }
-
-        [Console]::SetCursorPosition($finalLeft, $finalTop)
-        [Microsoft.PowerShell.PSConsoleReadLine]::Insert("")
+        Restore-Visuals -Info $info
     }
 }
 
