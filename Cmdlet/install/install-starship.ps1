0436d02819b340099d45de75742155252b70e053
metasong <metaseed@gmail.com>
2026-09-05 10:52:15 +0200

    fix(install): correct starship init path and force yazi shim hardlinks

--- Cmdlet/install/install-starship.ps1
+++ Cmdlet/install/install-starship.ps1
@@ -1,7 +1,7 @@
 # ipmo Metaseed.Management -Force
 Install-FromGithub https://github.com/starship/starship '-x86_64-pc-windows-msvc\.zip$' -versionType 'preview' @args
 # Add-PathEnv c:\app\ImageMagick
-$initFile = "$env:MS_PWSH\Profile\load\_prompt\starship\.config\starship-init.ps1"
+$initFile = "$env:MS_PWSH\Profile\load\prompt\starship\.config\starship-init.ps1"
 @"
 ## generated from:
 ## & 'C:\App\starship.exe' init powershell --print-full-init |code -
--- Cmdlet/install/install-yazi.ps1
+++ Cmdlet/install/install-yazi.ps1
@@ -2,7 +2,7 @@
 Install-FromGithub https://github.com/sxyazi/yazi 'x86_64-pc-windows-msvc\.zip$' -versionType 'stable' @args
 
 # Add-PathEnv 'C:\App\poppler-windows\Library\bin'
-New-Item -ItemType HardLink -Path $env:MS_App\_shim\ya.exe -Value c:\app\yazi\ya.exe
-New-Item -ItemType HardLink -Path $env:MS_App\_shim\yazi.exe -Value c:\app\yazi\yazi.exe
+New-Item -ItemType HardLink -Path $env:MS_App\_shim\ya.exe -Value c:\app\yazi\ya.exe -Force
+New-Item -ItemType HardLink -Path $env:MS_App\_shim\yazi.exe -Value c:\app\yazi\yazi.exe -Force
 
 Set-EnvVar YAZI_CONFIG_HOME $env:MS_PWSH\config\yazi
